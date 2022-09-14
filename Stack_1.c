6e062113457989b196628fd99e13fb555c738cfd
Halim Mallick <113496380+Halim-Mallick@users.noreply.github.com>
2022-09-15 01:29:54 +0600

    Update Stack_1.c
    It's updated

--- Stack_1.c
+++ Stack_1.c
@@ -1,44 +1,51 @@
+//POP, PUSH and PEEK in c
 #include<stdio.h>
-#define CAPACITY 3
-int stack[CAPACITY];
-int top = -1;
+#define MAXSIZE 4
+int stack[MAXSIZE];
+int top=-1;
 
 void push(int x){
-    if(top < CAPACITY - 1){
-        top = top + 1;
-        stack[top]= x;
-        printf("Succesfully added item: %d\n", x);
-    }else{
+    if(top<MAXSIZE-1){
+        top=top+1;
+        stack[top]=x;
+        printf("Item Added Successfully %d\n",x);
+    }
+    else{
         printf("Stack is Overflow\n");
     }
 }
 int pop(){
-    if(top >= 0){
-        int val = stack[top];
-        top = top - 1;
+    if(top>=0){
+        int val=stack[top];
+        top=top-1;
         return val;
     }
-    printf("Stack Is Empty\n");
-    return -1;
+    else{
+        printf("Stack is Empty");
+        return -1;
+    }
 }
 int peek(){
-    if(top >= 0){
+    if(top>=0){
         return stack[top];
     }
-    printf("Stack Is Empty\n");
-    return -1;
+    else{
+        printf("Stack Is Empty\n");
+        return -1;
+    }
 }
 //Main function
 int main(){
-    printf("Start From here.\n\n");
+    printf("\n.....Stack is Start......\n\n");
     peek();
     push(10);
     push(20);
     push(30);
+    printf("Pop Item %d\n",pop());
     push(40);
-    printf("Pop item: %d\n", pop());
-    push(40);
-    printf("Top of Stack: %d\n",peek());
-    push(100);
+    push(50);
+    printf("Top item is now %d\n",peek());
+    push(60);
+    push(70);
     return 0;
 }
